33d77d73509f7b475f885993974be4719ec8f31e
lapshuntanya <lapshun.tatiana@gmail.com>
2024-09-09 17:33:25 +0300

    +

--- P32-T25-OverloadingOperators/Date.cpp
+++ P32-T25-OverloadingOperators/Date.cpp
@@ -17,16 +17,24 @@ Date::Date()
 
 Date::Date(int year, int month, int day)
 {
+    this->year = year;
+    this->month = month;
+    this->day = day;
 }
 
 bool Date::isLeapYear() const
 {
-    return false;
+    return year%4 == 0&& year%100 != 0 || year%400 == 0;
 }
 
 int Date::monthDays() const
 {
-    return 0;
+    if (month == 1 || month == 3 || month == 5 || month == 7 ||
+        month == 8 || month == 10 || month == 12)
+        return 31;
+    else if (month == 2)
+        return isLeapYear() ? 29 : 28;
+    return 30;
 }
 
 void Date::nextDate()
